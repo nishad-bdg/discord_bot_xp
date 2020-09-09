de8079bf8c00a1a23612d747803f45cd7440be9b
Nishad Islam <nishad.bdg@gmail.com>
2020-09-09 22:53:45 +0600

    message channel updated

--- cogs/MyEvents.py
+++ cogs/MyEvents.py
@@ -17,13 +17,13 @@ class MyEvents(commands.Cog):
         print(f"We logegd in as : {self.bot.user}")
         print(f"version : {discord.__version__}")
 
+
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author == self.bot.user:
             return
         xp = 300
         users = self.bot.get_guild(config.SERVER_ID)
-
         await ux.UserExperience(self.bot.db, message, xp, self.bot).add_user_xp()
 
 
